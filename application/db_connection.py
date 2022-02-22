508d75489cf76d2a2b0fb92aca9c8da04da80059
Bo Yan <71803935+b3yan@users.noreply.github.com>
2022-02-21 20:53:31 -0800

    Delete db_connection.py

--- application/db_connection.py
+++
@@ -1,8 +0,0 @@
-import MySQLdb
-
-sql_server_string = "localhost"
-username = "root"
-password = "Tian1Tian2@"
-
-db = MySQLdb.connect(sql_server_string, username, password)
-URLLookupService/application/db_connection.py

f1a133a1491bf356a03e1273643ff6eb29823f20
Bo Yan <71803935+b3yan@users.noreply.github.com>
2022-02-21 20:52:59 -0800

    Add files via upload

---
+++ application/db_connection.py
@@ -0,0 +1,8 @@
+import MySQLdb
+
+sql_server_string = "localhost"
+username = "root"
+password = "Tian1Tian2@"
+
+db = MySQLdb.connect(sql_server_string, username, password)
+URLLookupService/application/db_connection.py

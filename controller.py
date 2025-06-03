db95fd7a18275663412346cbf3a4dd0e079c1a5f
Bo Yan <71803935+b3yan@users.noreply.github.com>
2025-06-03 10:43:14 -0400

    Add files via upload

---
+++ controller.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+from application.db_actions import get, update, insert
+
+application = Flask(__name__)
+@application.route('/urlinfo/1/<hostname_and_port>/<original_path_and_query_string>')
+def get_url(hostname_and_port, original_path_and_query_string):
+    malware_info = get(hostname_and_port)
+    if not malware_info: malware_info = "not malware"
+    return malware_info
+
+@application.route('/urlinfo', methods = ['POST'])
+def save_url():
+    url = request.form['url']
+    malware_info = request.form['malware_info']
+    return insert(url, malware_info)
---
+++ test.py
@@ -0,0 +1,22 @@
+from app.db_actions import get, update, insert
+
+def test_get():
+    get_value = get("https://en.wikipedia.org/wiki/Main_Page")
+    print(get_value)
+
+def test_update():
+    update(url = 'not_malware.com', malware_info = "not malware")
+
+def test_insert():
+    insert_value = insert("malware.com", "malware")
+    print(insert_value)
+
+def test_all():
+    insert("malware.com", "malware")
+    insert("not_malware.com", "not malware")
+    
+    value_1 = get("malware.com")
+    value_2 = get("not_malware.com")
+    
+    assert value_1 == "malware"    
+    assert value_2 == "not malware"
